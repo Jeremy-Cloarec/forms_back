16bd2c386b39f56e58d38d8b36529de6be84dcb2
Jérémy Cloarec <105861264+Jeremy-Cloarec@users.noreply.github.com>
2024-06-18 14:21:56 +0200

    fix bug with code mirror, build test

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,80 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiFormDeletionTraceFormDeletionTrace
+  extends Schema.CollectionType {
+  collectionName: 'form_deletion_traces';
+  info: {
+    singularName: 'form-deletion-trace';
+    pluralName: 'form-deletion-traces';
+    displayName: 'form-deletion-trace';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    originalCreated: Attribute.DateTime;
+    originalId: Attribute.Integer;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::form-deletion-trace.form-deletion-trace',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::form-deletion-trace.form-deletion-trace',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiPRVeloPRVelo extends Schema.CollectionType {
+  collectionName: 'p_r_velos';
+  info: {
+    singularName: 'p-r-velo';
+    pluralName: 'p-r-velos';
+    displayName: 'p-r-velo';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    statut: Attribute.Enumeration<
+      ['\u00C0 traiter', 'Trait\u00E9', 'Annul\u00E9', 'En attente']
+    >;
+    nom: Attribute.String;
+    prenom: Attribute.String;
+    date_de_naissance: Attribute.Date;
+    type_de_carte: Attribute.Enumeration<['T\u00E9c\u00E9ly', 'O\u00F9RA!']>;
+    numero_carte: Attribute.String;
+    fichier: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    commentaire: Attribute.Text;
+    fichiers: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::p-r-velo.p-r-velo',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::p-r-velo.p-r-velo',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -590,6 +664,53 @@ export interface PluginContentReleasesReleaseAction
   };
 }
 
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
+  info: {
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+          max: 50;
+        },
+        number
+      >;
+    code: Attribute.String & Attribute.Unique;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUsersPermissionsPermission
   extends Schema.CollectionType {
   collectionName: 'up_permissions';
@@ -741,127 +862,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
-  info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
-    description: '';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<
-        {
-          min: 1;
-          max: 50;
-        },
-        number
-      >;
-    code: Attribute.String & Attribute.Unique;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiFormDeletionTraceFormDeletionTrace
-  extends Schema.CollectionType {
-  collectionName: 'form_deletion_traces';
-  info: {
-    singularName: 'form-deletion-trace';
-    pluralName: 'form-deletion-traces';
-    displayName: 'form-deletion-trace';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    originalCreated: Attribute.DateTime;
-    originalId: Attribute.Integer;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::form-deletion-trace.form-deletion-trace',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::form-deletion-trace.form-deletion-trace',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiPRVeloPRVelo extends Schema.CollectionType {
-  collectionName: 'p_r_velos';
-  info: {
-    singularName: 'p-r-velo';
-    pluralName: 'p-r-velos';
-    displayName: 'p-r-velo';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    statut: Attribute.Enumeration<
-      ['\u00C0 traiter', 'Trait\u00E9', 'Annul\u00E9', 'En attente']
-    >;
-    nom: Attribute.String;
-    prenom: Attribute.String;
-    date_de_naissance: Attribute.Date;
-    type_de_carte: Attribute.Enumeration<['T\u00E9c\u00E9ly', 'O\u00F9RA!']>;
-    numero_carte: Attribute.String;
-    fichier: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    commentaire: Attribute.Text;
-    fichiers: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::p-r-velo.p-r-velo',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::p-r-velo.p-r-velo',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -872,16 +872,16 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::form-deletion-trace.form-deletion-trace': ApiFormDeletionTraceFormDeletionTrace;
+      'api::p-r-velo.p-r-velo': ApiPRVeloPRVelo;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
+      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'plugin::i18n.locale': PluginI18NLocale;
-      'api::form-deletion-trace.form-deletion-trace': ApiFormDeletionTraceFormDeletionTrace;
-      'api::p-r-velo.p-r-velo': ApiPRVeloPRVelo;
     }
   }
 }
